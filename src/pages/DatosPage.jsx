f7fbabcea577bcb82608cf8fac210199031442f3
CoderLozick <67139543+GermanLoz@users.noreply.github.com>
2021-09-22 03:23:52 -0300

    Update DatosPage.jsx

--- src/pages/DatosPage.jsx
+++ src/pages/DatosPage.jsx
@@ -4,12 +4,15 @@ import Table from '../components/admin/table.jsx'
 import Modal from '../components/admin/modal.jsx'
 import Buscador from '../components/search/buscador.jsx'
 import useAuth from '../customHooks/useAuth'
+import Pagination from '../components/pagination.jsx'
 
 const DatosPage = () => {
     const [modal, setModal] = useState(false)
     const [datosAdmin, setDatosAdmin] = useState([])
     const auth = useAuth()
     const matriz = auth.matriz()
+    const [paginaActual, setPaginaActual] = useState(1);
+    const [maximoDeTablas] = useState(20);
     
     useEffect(()=>{
         if(datos){
@@ -23,6 +26,13 @@ const DatosPage = () => {
             }
         }
     }, [])
+    
+    const indexOfLastTable = paginaActual * maximoDeTablas;
+    const indexOfFirstTable = indexOfLastTable - maximoDeTablas;
+    //Dividir array 
+    const datosParidos = datosAdmin.slice(indexOfFirstTable, indexOfLastTable);
+    //Paginar
+    const paginate = pageNumber => setPaginaActual(pageNumber);
 
     return(
         <>
@@ -56,6 +66,10 @@ const DatosPage = () => {
                     : ' '
                 }
             </div>
+            <Pagination maximoDeTablas={maximoDeTablas}
+                        tablas={datosAdmin.length}
+                        paginate={paginate}
+                       />
         </>
     )
 }
